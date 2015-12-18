3167ab6acc17bf7961e2abc58161b19607cb82a0
jktravis <joshua.travis@gmail.com>
2015-12-18 12:06:35 -0500

    Getting accurate data and populating the template appropriately.

--- static/js/main.js
+++ static/js/main.js
@@ -9,7 +9,12 @@ $(document).ready(function () {
   var html;
 
   $.getJSON('http://www.freecodecamp.com/news/hot', function(resp) {
-    context.entries = resp;
+    context.entries = resp.map(function(curr, index, arr) {
+      curr.timePosted = new Date(curr.timePosted).toISOString().replace(/T(.*)\..*/, ' $1');
+      curr.storyLink = 'http://www.freecodecamp.com/news/' + curr.storyLink.replace(/\s/g, '-');
+      curr.numVotes = curr.upVotes.length;
+      return curr;
+    });
     html = template(context);
     $('#feed').html(html);
   });
